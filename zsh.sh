b7ea9b0b01cde1d4f36b50b0dbb8cb440c2b563e
Carlos Johnson <carlos@quiklyn.com>
2019-10-22 17:20:22 +0300

    zsh history config

--- zsh.sh
+++ zsh.sh
@@ -1,6 +1,15 @@
 autoload -U compaudit compinit
+
 setopt auto_pushd pushd_ignore_dups pushdminus
+
 unsetopt menu_complete flowcontrol
 setopt auto_menu complete_in_word always_to_end
+
+[ -z "$HISTFILE" ] && HISTFILE="$HOME/.zsh_history"
+HISTSIZE=50000
+SAVEHIST=10000
+setopt extended_history hist_expire_dups_first hist_ignore_dups \
+	hist_ignore_space hist_verify inc_append_history share_history
+
 compinit
 
