382ffa78c2845cbdab9cc13b1cf59a5e9fb92bac
Carlos Johnson <carlos@quiklyn.com>
2019-10-24 14:27:41 +0300

    zsh dirs

--- zsh.sh
+++ zsh.sh
@@ -2,6 +2,20 @@ autoload -U compaudit compinit
 
 setopt auto_pushd pushd_ignore_dups pushdminus
 
+function d () {
+  if [[ -n $1 ]]; then
+    dirs "$@"
+  else
+    dirs -v | head -10
+  fi
+}
+compdef _dirs d
+
+alias lsa='ls -lah'
+alias l='ls -lah'
+alias ll='ls -lh'
+alias la='ls -lAh'
+
 unsetopt menu_complete flowcontrol
 setopt auto_menu complete_in_word always_to_end
 
