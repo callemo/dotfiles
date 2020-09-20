e237b1c0a8028a135631062039e799a80b542513
Carlos Johnson <carlos@quiklyn.com>
2020-09-20 17:29:38 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,21 +1,20 @@
-#
-# Shell initialization script
-#
-# Variables {{{
+# Initializes the current shell. Meant to be sourced.
+
 HISTSIZE=50000 export HISTSIZE
 PYTHONUSERBASE="$HOME/python"; export PYTHONUSERBASE
-_script_dir="$(dirname $0)"
-# }}}
-# Functions {{{
-_source_if() { [[ -r "${1}" ]] && . "${1}"; }
-_path_prepend_if() { [[ -d "${1}" ]] && PATH="${1}:${PATH}"; }
-_venv_prompt() { [[ "${VIRTUAL_ENV}" ]] && echo -n "($(basename "${VIRTUAL_ENV}")) "; }
+
+progdir="$(dirname "$0")"
+
+_source_if() { [ -r "$1" ] && . "$1"; }
+_path_prepend_if() { [ -d "$1" ] && PATH="$1:$PATH"; }
+_venv_prompt() { [ "$VIRTUAL_ENV" ] && echo "($(basename "$VIRTUAL_ENV")) "; }
+
 # Update current branch from master
 gupm () {
   local branch master
   master="${1:-master}"
   branch="$(git branch | awk '/^\*/ { print $2 }')"
-  if [[ -z "${branch}" ]] || [[ "${branch}" = "${master}" ]]; then
+  if [ -z "${branch}" ] || [ "${branch}" = "${master}" ]; then
     echo "bad branch: ${branch}" >&2
     return 1
   fi
@@ -27,7 +26,7 @@ gupm () {
 
 # Find all repos and update
 gupr () {
-  if [[ $# -lt 1 ]]; then
+  if [ $# -lt 1 ]; then
     echo "usage: $0 path [args]" >&2
     return 1
   fi
@@ -38,25 +37,21 @@ gupr () {
     | xargs -n 1 -I % git -C "%" pull --rebase "$@"
 }
 
-# }}}
-# Prompt {{{
-. "${_script_dir}/git-prompt.sh"
-if [[ -n "${ZSH_VERSION}" ]]; then
+. "$progdir/git-prompt.sh"
+if [ -n "$ZSH_VERSION" ]; then
   precmd () { __git_ps1 "$(_venv_prompt)%m:%1~" " %# "; }
 else
-  PROMPT_COMMAND='__git_ps1 "$(_venv_prompt)\h:\W" "\\\$ "'
-  export PROMPT_COMMAND
+  PROMPT_COMMAND='__git_ps1 "$(_venv_prompt)\h:\W" "\\\$ "'; export PROMPT_COMMAND
 fi
+
 GIT_PS1_SHOWDIRTYSTATE=1
 GIT_PS1_SHOWSTASHSTATE=1
 GIT_PS1_SHOWUNTRACKEDFILES=1
 GIT_PS1_SHOWUPSTREAM="auto"
 GIT_PS1_SHOWCOLORHINTS=1
-# }}}
-# Aliases {{{
+
 alias dco='docker-compose'
 alias dps='docker ps -a --format "table {{.Names}}\t{{.ID}}\t{{.Status}}\t{{.Ports}}"'
-
 alias ga='git add'
 alias gb='git branch'
 alias gca='git commit -v -a'
@@ -76,10 +71,8 @@ alias gmtvim='git mergetool --no-prompt --tool=vimdiff'
 alias gst='git status'
 alias gupav='git pull --rebase --autostash -v'
 alias gup='git pull --rebase'
+alias tsp='tmux split-window -l 12'
 
-alias tsw='tmux split-window -l 12'
-# }}}
-# PATH {{{
 _path_prepend_if '/usr/local/go/bin'
 _path_prepend_if '/usr/local/node/bin'
 _path_prepend_if "$PYTHONUSERBASE/bin"
@@ -87,8 +80,6 @@ _path_prepend_if "$HOME/dotfiles/bin"
 _path_prepend_if "$HOME/bin"
 export PATH
 
-# }}}
+[ -n "${ZSH_VERSION}" ] && _source_if "${progdir}/zsh.sh"
 
-[[ -n "${ZSH_VERSION}" ]] && _source_if "${_script_dir}/zsh.sh"
-unset _script_dir _path_prepend_if _source_if
-# vim: set sw=2 sts=2 et fdm=marker:
+unset progdir _path_prepend_if _source_if
