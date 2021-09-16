4e07080b0a76f588a9bc79107e6c237aa682d877
Carlos Johnson <carlos@quiklyn.com>
2021-09-16 20:53:01 +0100

    Deduplicate shell paths and configure command history

--- init.sh
+++ init.sh
@@ -1,35 +1,41 @@
-# Initializes the current shell. Meant to be sourced.
-_source_if() { [ -r "$1" ] && . "$1"; }
-_path_prepend_if() { [ -d "$1" ] && PATH="$1:$PATH"; }
-_venv_prompt() { [ "$VIRTUAL_ENV" ] && echo "($(basename "$VIRTUAL_ENV")) "; }
+#!/bin/bash
+
+_prepend_missing_path() {
+  case "$PATH" in
+  $1:*)
+    ;;
+  *:$1:*)
+    ;;
+  *:$1)
+    ;;
+  *)
+    if [ -d "$1" ]
+    then
+      PATH="$1:$PATH";
+    fi
+    ;;
+  esac
+}
 
 DOTFILES="${DOTFILES:-"$HOME/dotfiles"}"; export DOTFILES
 EDITOR=vim; export EDITOR
-HISTSIZE=50000 export HISTSIZE
+HISTSIZE=100000; export HISTSIZE
 PYTHONUSERBASE="$HOME/python"; export PYTHONUSERBASE
 
-_path_prepend_if '/usr/local/go/bin'
-_path_prepend_if '/usr/local/node/bin'
-_path_prepend_if "$PYTHONUSERBASE/bin"
-_path_prepend_if "$HOME/dotfiles/bin"
-_path_prepend_if "$HOME/bin"
-export PATH
-
-. "$DOTFILES/git-prompt.sh"
-GIT_PS1_SHOWDIRTYSTATE=1
-GIT_PS1_SHOWSTASHSTATE=1
-GIT_PS1_SHOWUNTRACKEDFILES=1
-GIT_PS1_SHOWUPSTREAM="auto"
-GIT_PS1_SHOWCOLORHINTS=1
-
-if [ -n "$ZSH_VERSION" ]; then
-  precmd () { __git_ps1 "$(_venv_prompt)%m:%1~" " %# "; }
+if [ -n "$ZSH_VERSION" ]
+then
+  SAVEHIST="$HISTSIZE"; export SAVEHIST
+  : "${HISTFILE:=$HOME/.zsh_history}"
+  bindkey -e  # implicitly changed with EDITOR=vim
 else
-  PROMPT_COMMAND='__git_ps1 "$(_venv_prompt)\h:\W" "\\\$ "'; export PROMPT_COMMAND
+  HISTFILESIZE="$HISTSIZE"; export HISTFILESIZE
 fi
 
-. "$DOTFILES/aliases.sh"
+_prepend_missing_path '/usr/local/go/bin'
+_prepend_missing_path '/usr/local/node/bin'
+_prepend_missing_path "$PYTHONUSERBASE/bin"
+_prepend_missing_path "$HOME/dotfiles/bin"
+_prepend_missing_path "$HOME/bin"
+export PATH
 
-[ -n "${ZSH_VERSION}" ] && . "$DOTFILES/zsh.sh"
-
-unset _path_prepend_if _source_if
+unset _prepend_missing_path
