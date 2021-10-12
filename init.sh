74a91ff8a6c34f07472d36687949096f18cdd2f5
Carlos Johnson <carlos@quiklyn.com>
2021-10-12 16:54:08 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,38 +1,33 @@
 #!/bin/bash
 
-_prepend_missing_path() {
-	case "$PATH" in
-	$1:*) ;;
-	*:$1:*) ;;
-	*:$1) ;;
-	*)
-		if [ -d "$1" ]
-		then
-			PATH="$1:$PATH";
-		fi
-		;;
-	esac
+_merge_path() {
+    case "$PATH" in
+    $1:*)   ;;
+    *:$1:*) ;;
+    *:$1)   ;;
+    *)      if [ -d "$1" ]; then PATH="$1:$PATH"; fi;;
+    esac
 }
 
 DOTFILES="${DOTFILES:-"$HOME/dotfiles"}"; export DOTFILES
-EDITOR=vim; export EDITOR
-HISTSIZE=100000; export HISTSIZE
-PYTHONUSERBASE="$HOME/python"; export PYTHONUSERBASE
+EDITOR=vim;                               export EDITOR
+HISTSIZE=100000;                          export HISTSIZE
+PYTHONUSERBASE="$HOME/python";            export PYTHONUSERBASE
 
 if [ -n "$ZSH_VERSION" ]
 then
-	SAVEHIST="$HISTSIZE"; export SAVEHIST
-	: "${HISTFILE:=$HOME/.zsh_history}"
-	bindkey -e  # implicitly changed with EDITOR=vim
+    SAVEHIST="$HISTSIZE"; export SAVEHIST
+    : "${HISTFILE:=$HOME/.zsh_history}"
+    bindkey -e  # implicitly changed with EDITOR=vim
 else
-	HISTFILESIZE="$HISTSIZE"; export HISTFILESIZE
+    HISTFILESIZE="$HISTSIZE"; export HISTFILESIZE
 fi
 
-_prepend_missing_path '/usr/local/go/bin'
-_prepend_missing_path '/usr/local/node/bin'
-_prepend_missing_path "$PYTHONUSERBASE/bin"
-_prepend_missing_path "$HOME/dotfiles/bin"
-_prepend_missing_path "$HOME/bin"
+_merge_path '/usr/local/go/bin'
+_merge_path '/usr/local/node/bin'
+_merge_path "$PYTHONUSERBASE/bin"
+_merge_path "$HOME/dotfiles/bin"
+_merge_path "$HOME/bin"
 export PATH
 
-unset _prepend_missing_path
+unset _merge_path
