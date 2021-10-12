086df58c86ce7ad0418c13cf3fbece02440aa894
Carlos Johnson <carlos@quiklyn.com>
2021-10-12 16:30:45 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
 _prepend_missing_path() {
-  case "$PATH" in
-  $1:*)
-    ;;
-  *:$1:*)
-    ;;
-  *:$1)
-    ;;
-  *)
-    if [ -d "$1" ]
-    then
-      PATH="$1:$PATH";
-    fi
-    ;;
-  esac
+	case "$PATH" in
+	$1:*) ;;
+	*:$1:*) ;;
+	*:$1) ;;
+	*)
+		if [ -d "$1" ]
+		then
+			PATH="$1:$PATH";
+		fi
+		;;
+	esac
 }
 
 DOTFILES="${DOTFILES:-"$HOME/dotfiles"}"; export DOTFILES
@@ -24,11 +21,11 @@ PYTHONUSERBASE="$HOME/python"; export PYTHONUSERBASE
 
 if [ -n "$ZSH_VERSION" ]
 then
-  SAVEHIST="$HISTSIZE"; export SAVEHIST
-  : "${HISTFILE:=$HOME/.zsh_history}"
-  bindkey -e  # implicitly changed with EDITOR=vim
+	SAVEHIST="$HISTSIZE"; export SAVEHIST
+	: "${HISTFILE:=$HOME/.zsh_history}"
+	bindkey -e  # implicitly changed with EDITOR=vim
 else
-  HISTFILESIZE="$HISTSIZE"; export HISTFILESIZE
+	HISTFILESIZE="$HISTSIZE"; export HISTFILESIZE
 fi
 
 _prepend_missing_path '/usr/local/go/bin'
