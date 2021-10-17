1612fdf0146d4fcade6f9e18493bddc65934967c
Carlos Johnson <carlos@quiklyn.com>
2021-10-17 17:14:38 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 _merge_path() {
-    case "$PATH" in
-    $1:*)   ;;
-    *:$1:*) ;;
-    *:$1)   ;;
-    *)      if [ -d "$1" ]; then PATH="$1:$PATH"; fi;;
-    esac
+	case "$PATH" in
+	$1:*)	;;
+	*:$1:*) ;;
+	*:$1)	;;
+	*)		if [ -d "$1" ]; then PATH="$1:$PATH"; fi;;
+	esac
 }
 
 DOTFILES="${DOTFILES:-"$HOME/dotfiles"}"; export DOTFILES
-EDITOR=vim;                               export EDITOR
-HISTSIZE=100000;                          export HISTSIZE
+EDITOR=vim; export EDITOR
+HISTSIZE=100000; export HISTSIZE
 
 if [ -n "$ZSH_VERSION" ]
 then
-    SAVEHIST="$HISTSIZE"; export SAVEHIST
-    : "${HISTFILE:=$HOME/.zsh_history}"
-    bindkey -e  # implicitly changed with EDITOR=vim
+	SAVEHIST="$HISTSIZE"; export SAVEHIST
+	: "${HISTFILE:=$HOME/.zsh_history}"
+	bindkey -e	# implicitly changed with EDITOR=vim
 else
-    HISTFILESIZE="$HISTSIZE"; export HISTFILESIZE
+	HISTFILESIZE="$HISTSIZE"; export HISTFILESIZE
 fi
 
 _merge_path '/usr/local/node/bin'
